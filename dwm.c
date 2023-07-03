5b22c1c1a85c635b166599e96ae04185972d7051
pankx <a3427173515@163.com>
2023-07-03 23:33:15 +0800

    编译警告 logtofile 未使用

--- dwm.c
+++ dwm.c
@@ -190,7 +190,7 @@ struct Systray {
 };
 
 /* function declarations */
-static void logtofile(const char *fmt, ...);
+//static void logtofile(const char *fmt, ...);
 
 static void tile(Monitor *m);
 static void magicgrid(Monitor *m);
@@ -407,20 +407,20 @@ struct Pertag {
 };
 
 /* function implementations */
-void
-logtofile(const char *fmt, ...)
-{
-    char buf[256];
-    char cmd[256];
-    va_list ap;
-    va_start(ap, fmt);
-    vsprintf((char *)buf, fmt, ap);
-    va_end(ap);
-    uint i = strlen((const char *)buf);
-
-    sprintf(cmd, "echo '%.*s' >> ~/log", i, buf);
-    system(cmd);
-}
+//void
+//logtofile(const char *fmt, ...)
+//{
+//    char buf[256];
+//    char cmd[256];
+//    va_list ap;
+//    va_start(ap, fmt);
+//    vsprintf((char *)buf, fmt, ap);
+//    va_end(ap);
+//    uint i = strlen((const char *)buf);
+//
+//    sprintf(cmd, "echo '%.*s' >> ~/log", i, buf);
+//    system(cmd);
+//}
 
 void
 applyrules(Client *c)
