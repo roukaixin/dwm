5bfd40c92dc8404e5f949e1e1fa01bf85c097bcd
pankx <a3427173515@163.com>
2023-07-05 22:59:47 +0800

    restore SIGCHLD sighandler to default before spawning a program

--- dwm.c
+++ dwm.c
@@ -2636,10 +2636,18 @@ sigchld(int unused)
 void
 spawn(const Arg *arg)
 {
+    struct sigaction sa;
+
     if (fork() == 0) {
         if (dpy)
             close(ConnectionNumber(dpy));
         setsid();
+
+        sigemptyset(&sa.sa_mask);
+        sa.sa_flags = 0;
+        sa.sa_handler = SIG_DFL;
+        sigaction(SIGCHLD, &sa, NULL);
+
         execvp(((char **)arg->v)[0], (char **)arg->v);
         fprintf(stderr, "dwm: execvp %s", ((char **)arg->v)[0]);
         perror(" failed");
